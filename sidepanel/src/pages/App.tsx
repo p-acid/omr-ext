49660ddaae7e501e1fa9abe9d213630804a34d8c
p-acid <qkrtkstjd01@gmail.com>
2025-05-10 16:23:12 +0900

    chore: add daisyui

--- sidepanel/src/pages/App.tsx
+++ sidepanel/src/pages/App.tsx
@@ -3,7 +3,7 @@ function App() {
     <main className="flex min-h-screen flex-col items-center justify-center bg-zinc-950">
       <h1 className="text-2xl font-extrabold text-white">Hello World !!</h1>
       <button
-        className="h-10 rounded-md bg-green-500 px-3 text-white"
+        className="btn btn-md btn-primary"
         onClick={() => alert("Hello World !!")}
       >
         Alert!!
